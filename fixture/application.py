ed1d345a56c18e982f9382a5748e40c90fb19a88
enibeni <enibeni@ya.ru>
2016-10-27 17:06:14 +0300

    add group managment class

--- fixture/application.py
+++ fixture/application.py
@@ -1,33 +1,15 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
 class Application:
-    def __init__(self) -> object:
+    def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-    def check_groups(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def add_group(self, group):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.name)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.header)
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.check_groups()
+        self.group = GroupHelper(self)
 
     def open_browser(self):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,13 +14,13 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.add_group(Group(name="sdfg", header="sdfg", footer="sdfg"))
+    app.group.create(Group(name="sdfg", header="sdfg", footer="sdfg"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.add_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
 if __name__ == '__main__':
