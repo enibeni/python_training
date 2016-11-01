b201fa8632fd26ea70913684473340d46cb0096f
enibeni <enibeni@ya.ru>
2016-11-01 10:57:30 +0300

    add precondition

--- fixture/group.py
+++ fixture/group.py
@@ -46,3 +46,9 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_groups_page()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
