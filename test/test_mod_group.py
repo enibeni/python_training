cf4ab936efffd26ee217bfd3528b0b50c6712923
enibeni <enibeni@ya.ru>
2016-11-02 12:56:33 +0300

    too long without commits

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -2,8 +2,15 @@ from model.group import Group
 
 
 def test_mod_group_name(app):
-    app.group.mod_first_group(Group(name="new group"))
+    old_group = app.group.get_group_list()
+    group = Group(name="new group")
+    group.id = old_group[0].id
+    app.group.mod_first_group(group)
+    new_group = app.group.get_group_list()
+    assert len(old_group) == len(new_group)
+    old_group[0] = group
+    assert sorted(old_group, key = Group.id_or_max) == sorted(new_group, key = Group.id_or_max)
 
 
-def test_mod_group_header(app):
-    app.group.mod_first_group(Group(header="new header"))
+# def test_mod_group_header(app):
+#     app.group.mod_first_group(Group(header="new header"))
