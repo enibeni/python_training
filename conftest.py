753d3a5bf66257d7a19fd7c2d849eff3765e7d5a
enibeni <enibeni@ya.ru>
2016-11-17 15:30:06 +0300

    db fixture

--- conftest.py
+++ conftest.py
@@ -1,21 +1,43 @@
+import importlib
+import json
+import os
+import jsonpickle as jsonpickle
 import pytest
 from fixture.application import Application
+from fixture.db import DbFixture
 
 fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as config_file:
+            target = json.load(config_file)
+    return target
 
 
 @pytest.fixture  # Инициализация фикстуры
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:  # Самый первый раз
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():  # Создае заново если сломалась
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    web_config = load_config(request.config.getoption("--target"))["web"]
+    if fixture is None or not fixture.is_valid():  # Самый первый раз
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption('--target'))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)  # Финализация в конце всех тестов
@@ -23,12 +45,33 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
 
+
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default = "firefox")
-    parser.addoption("--baseUrl", action="store", default = "http://localhost/addressbook/")
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
 
 
 if __name__ == '__main__':
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,38 +1,13 @@
-import pytest
-import string
-import random
 from model.group import Group
 
-def random_string(prefix, maxlem):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlem))])
 
-
-testdata = [Group(name="", header="header", footer="footer")] + [
-        Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-        for i in range(5)
-]
-
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_mod_random_group_name(app, group):
-    old_groups = app.group.get_group_list()
+def test_mod_random_group_name(app, db, json_groups):
+    group = json_groups
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count() # Хэш фунция
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(new_groups, key = Group.id_or_max) == sorted(old_groups, key = Group.id_or_max)
-
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
 
 
 
